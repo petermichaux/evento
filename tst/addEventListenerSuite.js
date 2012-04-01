e305fc3c19e4e9fc0224c216edf4a939efbc77d4
Peter Michaux <petermichaux@gmail.com>
2012-03-31 23:05:27 -0700

    more buster setup

---
+++ tst/addEventListenerSuite.js
@@ -0,0 +1,11 @@
+(function() {
+
+    buster.testCase('addEventListenerSuite', {
+
+        "test set starts with no elements": function() {
+            assert.same(true, true);
+        }
+
+    });
+
+}());
