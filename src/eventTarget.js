c6ec9485432c4f1c711d9448a29c504ac91448c5
Peter Michaux <petermichaux@gmail.com>
2012-04-04 20:30:50 -0700

    Clean up eventTarget.js code. Add docs and license.

--- src/eventTarget.js
+++ src/eventTarget.js
@@ -1,3 +1,104 @@
+/*
+ * Copyright (c) 2012, Peter Michaux, http://peter.michaux.ca/
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+
+@property LIB_addEventListener
+
+@parameter element {EventTarget} The DOM element you'd like to observe.
+
+@parameter type {string} The name of the event.
+
+@parameter listener {object|function} The listener object or callback function.
+
+@parameter auxArg {string|object} Optional. See description.
+
+@description
+
+If the listener is an object then when a matching event type is dispatched on
+the event target, the listener object's handleEvent method will be called.
+Using the auxArg you can specify the name of the method to be called.
+
+If the listener is a function then when a matching event type is dispatched on
+the event target, the listener function is called with event target object set as
+the "this" object. Using the auxArg you can specifiy a different object to be
+the "this" object.
+
+One listener (or type/listener/auxArg pair to be more precise) can be added
+only once.
+
+LIB_addEventListener(document.body, 'click', {handleEvent:function(){}});
+LIB_addEventListener(document.body, 'click', {handleClick:function(){}}, 'handleClick');
+LIB_addEventListener(document.body, 'click', function(){});
+LIB_addEventListener(document.body, 'click', this.handleClick, this);
+
+*/
+
+/**
+
+@property LIB_removeEventListener
+
+@parameter element {EventTarget} The DOM element you'd like to stop observing.
+
+@parameter type {string} The name of the event.
+
+@parameter listener {object|function} The listener object or callback function.
+
+@parameter auxArg {string|object} Optional.
+
+@description
+
+Removes added listener matching the element/type/listener/auxArg combination exactly.
+If this combination is not found there are no errors.
+
+var o = {handleEvent:function(){}, handleClick:function(){}};
+LIB_removeEventListener(document.body, 'click', o);
+LIB_removeEventListener(document.body, 'click', o, 'handleClick');
+LIB_removeEventListener(document.body, 'click', fn);
+LIB_removeEventListener(document.body, 'click', this.handleClick, this);
+
+*/
+
+/**
+
+@property LIB_purgeEventListeners
+
+@parameter element {EventTarget} The DOM element you'd like to purge.
+
+@description
+
+Removes all event listeners added to the element and all its descendent elements
+through LIB_addEventListener. This purging should be done before your application
+code looses its last reference to the element. Otherwise memory will leak
+as this library will hold references to the element, the listener, and
+auxArg (if it was used) and all objects referenced by those objects through
+property or closure references.
+
+*/
+
 var LIB_addEventListener;
 var LIB_removeEventListener;
 var LIB_purgeEventListeners;
@@ -39,39 +140,16 @@ var LIB_purgeEventListeners;
                  (a.auxArg === b.auxArg)));
     }
 
-    // TODO rename getEventListener and don't return false
-    function hasEventListener(listeners, o) {
-        for (var i = 0, ilen = listeners.length; i < ilen; i++) {
-            var listener = listeners[i];
-            if (listenersAreEqual(listener, o)) {
-                return listener;
-            }
-        }
-        return false;
-    }
-
-    function removeEventListener(listeners, o) {
-        // Loop backwards through the array so adjacent references
-        // to "listener" are all removed.
-        for (var i = listeners.length; i--; ) {
-            if (listenersAreEqual(listeners[i], o)) {
-                listeners.splice(i, 1);
-            }
-        }
-    }
-
     var listeners = [];
 
-    var getElementListeners = function(element) {
-        var result = [];
-        for (var i=0, ilen=listeners.length; i<ilen; i++) {
-            var listener = listeners[i];
-            if (listener.element === element) {
-                result.push(listener);
+    function indexOfListener(o) {
+        for (var i = 0, ilen = listeners.length; i < ilen; i++) {
+            if (listenersAreEqual(listeners[i], o)) {
+                return i;
             }
         }
-        return result;
-    };
+        return -1;
+    }
 
     var makeAdder = function(guts) {
         return function(element, type, listener, /*optional*/ auxArg) {
@@ -79,7 +157,7 @@ var LIB_purgeEventListeners;
             // that were passed to this function. Using apply preserves
             // the number of arguments.
             var o = createListener.apply(null, arguments);
-            if (hasEventListener(listeners, o)) {
+            if (indexOfListener(o) >= 0) {
                 // do not add the same listener twice
                 return;
             }
@@ -90,16 +168,16 @@ var LIB_purgeEventListeners;
 
     var makeRemover = function(guts) {
         return function(element, type, listener, /*optional*/ auxArg) {
-            var o = createListener.apply(null, arguments);
-            if (o = hasEventListener(listeners, o)) {
-                guts(o);
-                removeEventListener(listeners, o); // TODO not efficient because already looked for it with hasEventListener
+            var i = indexOfListener(createListener.apply(null, arguments));
+            if (i >= 0) {
+                guts(listeners[i]);
+                listeners.splice(i, 1);
             }
         };
     };
 
-    if (typeof document.addEventListener === 'function' &&
-        typeof document.removeEventListener === 'function') {
+    if ((typeof document.addEventListener === 'function') &&
+        (typeof document.removeEventListener === 'function')) {
 
         LIB_addEventListener = makeAdder(function(o) {
             o.element.addEventListener(o.type, o.wrappedHandler, false);
@@ -110,10 +188,10 @@ var LIB_purgeEventListeners;
         });
 
     }
-    else if (typeof document.attachEvent === 'object' &&
-             document.attachEvent !== null &&
-             typeof document.detachEvent === 'object' &&
-             document.detachEvent !== null) {
+    else if ((typeof document.attachEvent === 'object') &&
+             (document.attachEvent !== null) &&
+             (typeof document.detachEvent === 'object') &&
+             (document.detachEvent !== null)) {
 
         LIB_addEventListener = makeAdder(function(o) {
             o.element.attachEvent('on'+o.type, o.wrappedHandler);
@@ -126,6 +204,19 @@ var LIB_purgeEventListeners;
     }
 
     if (typeof LIB_removeEventListener === 'function') {
+
+        // An inelegant and inefficient purge algorithm.
+
+        var getElementListeners = function(element) {
+            var result = [];
+            for (var i = 0, ilen = listeners.length; i < ilen; i++) {
+                var listener = listeners[i];
+                if (listener.element === element) {
+                    result.push(listener);
+                }
+            }
+            return result;
+        };
 
         var purge = LIB_purgeEventListeners = function(element) {
             var listeners = getElementListeners(element);
