c392838661873cd5425000816e23cb3d53c7438b
Peter Michaux <petermichaux@gmail.com>
2012-04-27 11:05:57 -0700

    add header.js file for concatenatio process

---
+++ src/header.js
@@ -0,0 +1,7 @@
+/*
+Evento version 0 - JavaScript libraries for working with the observer pattern
+Copyright (c) 2012, Peter Michaux
+All rights reserved.
+Licensed under the Simplified BSD License.
+https://github.com/petermichaux/evento/blob/master/LICENSE
+*/
