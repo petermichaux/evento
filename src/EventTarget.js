42cfd7d29b139566c6ff6771395cf370965d384a
Peter Michaux <petermichaux@gmail.com>
2013-06-05 21:05:04 -0700

    add superConstructor property

--- src/EventTarget.js
+++ src/EventTarget.js
@@ -10,7 +10,7 @@ The methods of an event target object are inspired by the DOM2 standard.
 
 */
 evento.EventTarget = function() {};
-// evento.EventTarget.superConstructor = Object;
+evento.EventTarget.superConstructor = Object;
 
 (function() {
 
